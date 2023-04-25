82eddaef0ce127798ad018fc99a48efde2ca6bf5
zoe <kny0651@naver.com>
2023-04-25 19:23:38 +0900

    refactor: 최초검색어저장이안되는문제 해결

--- HighwayInfo/Data/DefaultUserRepository.swift
+++ HighwayInfo/Data/DefaultUserRepository.swift
@@ -13,34 +13,33 @@ final class DefaultUserRepository: UserRepository {
     private let locationInfoKey = "locationInfo"
     
     func saveHistory(with locationInfo: LocationInfo) {
-        if let data = userDefault.data(forKey: locationInfoKey) {
-            do {
-                var info: [LocationInfo] = []
-                info = try JSONDecoder().decode([LocationInfo].self, from: data)
-                info.reverse()
-                info.append(locationInfo)
-                info.reverse()
-                let encoded = try JSONEncoder().encode(info)
-                userDefault.set(encoded, forKey: locationInfoKey)
-            } catch {
-                print(error)
-            }
-        } else {
-            // 처음저장할때 여기탈텐데
-            print("처음저장할 때")
+        guard let data = userDefault.data(forKey: locationInfoKey) else {
+            saveFirstLocation(locationInfo)
+            return
         }
+        guard var info = try? JSONDecoder().decode([LocationInfo].self, from: data) else {
+            return
+        }
+        info.reverse()
+        info.append(locationInfo)
+        guard let encoded = try? JSONEncoder().encode(info.reversed()) else {
+            return
+        }
+        userDefault.set(encoded, forKey: locationInfoKey)
     }
     
     func fetchSearchHistory() -> Observable<[LocationInfo]> {
-        var info: [LocationInfo] = []
-        guard let data = userDefault.data(forKey: locationInfoKey) else {
+        guard let data = userDefault.data(forKey: locationInfoKey),
+              let decoded = try? JSONDecoder().decode([LocationInfo].self, from: data) else {
             return Observable.of([])
         }
-        do {
-            info = try JSONDecoder().decode([LocationInfo].self, from: data)
-        } catch {
-            print(error)
+        return Observable.of(decoded)
+    }
+    
+    private func saveFirstLocation(_ locationInfo: LocationInfo) {
+        guard let encoded = try? JSONEncoder().encode([locationInfo]) else {
+            return
         }
-        return Observable.of(info)
+        userDefault.set(encoded, forKey: locationInfoKey)
     }
 }
