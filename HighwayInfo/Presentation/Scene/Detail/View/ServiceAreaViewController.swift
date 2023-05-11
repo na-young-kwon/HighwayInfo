aeda4a32849c68f4ac056da3dd7bf98a8b4e38d6
zoe <kny0651@naver.com>
2023-05-11 17:29:57 +0900

    refactor: ServiceAreaView 컬렉션뷰 2개로분리

--- HighwayInfo/Presentation/Scene/Detail/View/ServiceAreaViewController.swift
+++ HighwayInfo/Presentation/Scene/Detail/View/ServiceAreaViewController.swift
@@ -10,15 +10,19 @@ import RxSwift
 import RxCocoa
 
 final class ServiceAreaViewController: UIViewController {
-    enum Section: Int, CaseIterable {
-        case title
-        case list
+    enum TitleSection: Int, CaseIterable {
+        case main
+    }
+    enum FacilitySection: Int, CaseIterable {
+        case main
     }
     @IBOutlet weak var titleLabel: UILabel!
     var viewModel: ServiceAreaViewModel!
-    private var collectionView: UICollectionView!
+    private var titleCollectionView: UICollectionView!
+    private var facilityCollectionView: UICollectionView!
     private let disposeBag = DisposeBag()
-    private var dataSource: UICollectionViewDiffableDataSource<Section, AnyHashable>!
+    private var titleDataSource: UICollectionViewDiffableDataSource<TitleSection, Convenience>!
+    private var facilityDataSource: UICollectionViewDiffableDataSource<FacilitySection, ServiceArea>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,39 +32,37 @@ final class ServiceAreaViewController: UIViewController {
     }
     
     private func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createTitleLayout())
-        collectionView.alwaysBounceVertical = false
-        collectionView.showsVerticalScrollIndicator = false
-        view.addSubview(collectionView)
-        collectionView.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, bottom: view.bottomAnchor ,right: view.rightAnchor,
-                              paddingTop: 15, paddingLeft: 10, paddingBottom: 10, paddingRight: 10)
+        titleCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createTitleLayout())
+        titleCollectionView.alwaysBounceVertical = false
+        titleCollectionView.showsVerticalScrollIndicator = false
+        view.addSubview(titleCollectionView)
+        titleCollectionView.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                              paddingTop: 15, paddingLeft: 10, paddingRight: 10)
+        titleCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1).isActive = true
+        
+        facilityCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createListLayout())
+        facilityCollectionView.showsVerticalScrollIndicator = false
+        view.addSubview(facilityCollectionView)
+        facilityCollectionView.anchor(top: titleCollectionView.bottomAnchor, left: view.leftAnchor, bottom: view.bottomAnchor ,right: view.rightAnchor,
+                              paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10)
     }
     
     private func createTitleLayout() -> UICollectionViewLayout {
-        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            guard let sectionKind = Section(rawValue: sectionIndex) else { return nil }
-            let section: NSCollectionLayoutSection
-            if sectionKind == .title {
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalWidth(0.22))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-                section = NSCollectionLayoutSection(group: group)
-                section.orthogonalScrollingBehavior = .continuous
-                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20)
-            } else if sectionKind == .list {
-                var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-                configuration.showsSeparators = false
-                section = NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: layoutEnvironment)
-            } else {
-                fatalError("Unknown section")
-            }
-            return section
-        }
-        let config = UICollectionViewCompositionalLayoutConfiguration()
-        config.interSectionSpacing = 20
-        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider, configuration: config)
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalWidth(0.22))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func createListLayout() -> UICollectionViewLayout {
+        var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        configuration.showsSeparators = false
+        return UICollectionViewCompositionalLayout.list(using: configuration)
     }
     
     private func configureDataSource() {
@@ -70,15 +72,19 @@ final class ServiceAreaViewController: UIViewController {
         let serviceListCell = UICollectionView.CellRegistration<ServiceListCell, ServiceArea> { (cell, indexPath, data) in
             cell.bindViewModel(with: data)
         }
-        dataSource = UICollectionViewDiffableDataSource<Section, AnyHashable>(collectionView: collectionView) {
-            (collectionView: UICollectionView, indexPath: IndexPath, item: AnyHashable) -> UICollectionViewCell? in
-            if let convenience = item as? Convenience {
-                return collectionView.dequeueConfiguredReusableCell(using: convenienceCellRegistration, for: indexPath, item: convenience)
-            } else if let serviceArea = item as? ServiceArea {
-                return collectionView.dequeueConfiguredReusableCell(using: serviceListCell, for: indexPath, item: serviceArea)
-            }
-            return nil
+        titleDataSource = UICollectionViewDiffableDataSource<TitleSection, Convenience>(collectionView: titleCollectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: Convenience) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: convenienceCellRegistration, for: indexPath, item: item)
         }
+        facilityDataSource = UICollectionViewDiffableDataSource<FacilitySection, ServiceArea>(collectionView: facilityCollectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: ServiceArea) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: serviceListCell, for: indexPath, item: item)
+        }
+        var snapshot = NSDiffableDataSourceSnapshot<TitleSection, Convenience>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Convenience.allCases)
+        titleDataSource.apply(snapshot, animatingDifferences: false)
+        titleCollectionView.selectItem(at: IndexPath(item: 0, section: 0), animated: false, scrollPosition: .left)
     }
     
     private func bindViewModel() {
@@ -89,34 +95,30 @@ final class ServiceAreaViewController: UIViewController {
                                                selectedCategory: selectedCategory.asObservable(),
                                                selectedServiceArea: selectedServiceArea.asObservable())
         let output = viewModel.transform(input: input)
-        let serviceArea = Observable.zip(output.serviceArea, selectedCategory)
         
         output.highwayName
             .drive(titleLabel.rx.text)
             .disposed(by: disposeBag)
         
-        serviceArea
+        output.serviceArea
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] serviceZip in
                 self?.applySnapShot(with: serviceZip)
             })
             .disposed(by: disposeBag)
         
-        collectionView.rx.itemSelected
+        titleCollectionView.rx.itemSelected
             .subscribe(onNext: { [weak self] index in
-                let convenience = self?.dataSource.itemIdentifier(for: index)
-                guard let convenience = convenience as? Convenience else {
+                guard let convenience = self?.titleDataSource.itemIdentifier(for: index) else {
                     return
                 }
                 selectedCategory.onNext(convenience)
-                self?.collectionView.selectItem(at: IndexPath(item: convenience.rawValue, section: 0), animated: false, scrollPosition: .left)
             })
             .disposed(by: disposeBag)
         
-        collectionView.rx.itemSelected
+        facilityCollectionView.rx.itemSelected
             .subscribe(onNext: { [weak self] index in
-                let serviceArea = self?.dataSource.itemIdentifier(for: index)
-                guard let serviceArea = serviceArea as? ServiceArea else {
+                guard let serviceArea = self?.facilityDataSource.itemIdentifier(for: index) else {
                     return
                 }
                 selectedServiceArea.onNext(serviceArea)
@@ -124,14 +126,11 @@ final class ServiceAreaViewController: UIViewController {
             .disposed(by: disposeBag)
     }
     
-    private func applySnapShot(with service: ([ServiceArea], Convenience)) {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, AnyHashable>()
-        snapshot.appendSections([.title])
-        snapshot.appendItems(Convenience.allCases, toSection: .title)
-        snapshot.appendSections([.list])
-        snapshot.appendItems(service.0, toSection: .list)
-        dataSource.apply(snapshot, animatingDifferences: true)
-        collectionView.selectItem(at: IndexPath(item: service.1.rawValue, section: 0), animated: false, scrollPosition: .left)
+    private func applySnapShot(with service: [ServiceArea]) {
+        var snapshot = NSDiffableDataSourceSnapshot<FacilitySection, ServiceArea>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(service)
+        facilityDataSource.apply(snapshot, animatingDifferences: true)
     }
     
     deinit {
