b87555ba93012da2dd4eff45d3bb9e993c47b094
zoe <kny0651@naver.com>
2023-06-20 21:45:09 +0900

    chore: 사용하지않는 주석 제거

--- HighwayInfo/Application/SceneDelegate.swift
+++ HighwayInfo/Application/SceneDelegate.swift
@@ -13,21 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-//        let navigationController = UINavigationController()
         self.window = UIWindow(windowScene: windowScene)
-
-        guard let window = window else {
-            return
-        }
+        guard let window = window else { return }
         
         appCoordinator = AppCoordinator(window: window)
         appCoordinator?.start()
-        
-//        window = UIWindow(windowScene: windowScene)
-//        window?.rootViewController = navigationController
-//        window?.makeKeyAndVisible()
     }
 }
